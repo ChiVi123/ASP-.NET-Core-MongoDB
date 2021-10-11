e98c2d88563c7b7e91e96e09452fb1bcb1f2222e
ChiVi123 <nguyenhoangchivi@gmail.com>
2021-10-12 00:04:06 +0700

    demo cong nghe

---
+++ CoreMongoDB/CoreMongoDB/IRepository/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using CoreMongoDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMongoDB.IRepository
+{
+    public interface IEmployeeRepository
+    {
+        Employee Save(Employee employee);
+        Employee Get(string employeeId);
+        List<Employee> Gets();
+        string Delete(string employeeId);
+    }
+}
---
+++ CoreMongoDB/CoreMongoDB/Repository/EmployeeRepository.cs
@@ -0,0 +1,54 @@
+﻿using CoreMongoDB.IRepository;
+using CoreMongoDB.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMongoDB.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private MongoClient _mongoClient = null;
+        private IMongoDatabase _database = null;
+        private IMongoCollection<Employee> _employeeTable = null;
+
+        public EmployeeRepository()
+        {
+            _mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+            _database = _mongoClient.GetDatabase("OfficeDB");
+            _employeeTable = _database.GetCollection<Employee>("Employees");
+        }
+
+        public string Delete(string employeeId)
+        {
+            _employeeTable.DeleteOne(x => x.Id == employeeId);
+            return "Delete";
+        }
+
+        public Employee Get(string employeeId)
+        {
+            return _employeeTable.Find(x => x.Id == employeeId).FirstOrDefault();
+        }
+
+        public List<Employee> Gets()
+        {
+            return _employeeTable.Find(FilterDefinition<Employee>.Empty).ToList();
+        }
+
+        public Employee Save(Employee employee)
+        {
+            var emObj = _employeeTable.Find(x => x.Id == employee.Id).FirstOrDefault();
+            if (emObj == null)
+            {
+                _employeeTable.InsertOne(employee);
+            }
+            else
+            {
+                _employeeTable.ReplaceOne(x => x.Id == employee.Id, employee);
+            }
+            return employee;
+        }
+    }
+}
