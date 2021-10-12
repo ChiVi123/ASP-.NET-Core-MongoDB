ff97f25a9a04f748a11299be0c1925a34cd9759d
19110254@student.hcmute.edu.vn <19110254@student.hcmute.edu.vn>
2021-10-12 23:18:19 +0700

    thaydoi

---
+++ UserDemo/UserDemo/Controllers/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserDemo.Controllers
+{
+    public class User
+    {
+        public string ID { set; get; }
+       
+    }
+}
