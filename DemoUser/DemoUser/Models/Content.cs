ab5f063f9a2b5fe5a517b3176d8e112fa2d92ba9
19110254@student.hcmute.edu.vn <19110254@student.hcmute.edu.vn>
2021-11-29 19:59:09 +0700

    them content

---
+++ DemoUser/DemoUser/Models/Content.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoUser.Models
+{
+    public class Content
+    {
+       
+            [BsonId]
+            public ObjectId Id { get; set; }
+
+            [BsonElement]
+            public string title { get; set; }
+
+            [BsonElement]
+            public string note { get; set; }
+
+            [BsonElement]
+            public DateTime createdate { get; set; }
+
+            [BsonElement]
+            public string authorid { get; set; }
+        }
+    }
+
